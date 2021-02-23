ddf3da57986f0ed1239261a404921f7e035821e5
SheepishShepherd <cameronscottmcdougal@gmail.com>
2021-02-23 00:18:35 -0500

    Reset cooldown after every party to prevent party girl from spawning mid-party if off cooldown and party girl dies during party

--- ReliablePartyGirl.cs
+++ ReliablePartyGirl.cs
@@ -23,25 +23,28 @@ namespace ReliablePartyGirl
 				SpawnPartyGirlReady = true;
 			}
 			
-			// If a genuine party is occuring and we can spawn a new party girl, find an existing Party Girl if off cooldown
+			// If a genuine party is occuring and there is no existing Party Girl, attempt to spawn one
 			// GenuineParty assumes PartyIsUp is true
-			if (BirthdayParty.GenuineParty && SpawnPartyGirlReady && NPC.FindFirstNPC(NPCID.PartyGirl) < 0) {
-				// Spawn a smoke cloud
-				for (int j = 0; j < 4; j++) {
-					Utils.PoofOfSmoke(new Vector2(Main.spawnTileX * 16, Main.spawnTileY * 16));
+			if (BirthdayParty.GenuineParty && NPC.FindFirstNPC(NPCID.PartyGirl) < 0) {
+				// If not on cooldown, spawn the Party Girl
+				if (SpawnPartyGirlReady) {
+					// Spawn a smoke cloud
+					for (int j = 0; j < 4; j++) {
+						Utils.PoofOfSmoke(new Vector2(Main.spawnTileX * 16, Main.spawnTileY * 16));
+					}
+
+					// Create a new Party Girl NPC
+					int partyGirl = NPC.NewNPC(Main.spawnTileX * 16, Main.spawnTileY * 16, NPCID.PartyGirl);
+
+					// Broadcast that a Party Girl has arrived
+					string npcName = Main.npc[partyGirl].GivenName;
+					string npcTitle = Main.npc[partyGirl].TypeName;
+					Color pink = new Color(255, 110, 160);
+					WorldGen.BroadcastText(NetworkText.FromLiteral($"{npcName} the {npcTitle} has arrived to the party!"), pink);
 				}
 
-				// Create a new Party Girl NPC
-				int partyGirl = NPC.NewNPC(Main.spawnTileX * 16, Main.spawnTileY * 16, NPCID.PartyGirl);
-
-				// Broadcast that a Party Girl has arrived
-				string npcName = Main.npc[partyGirl].GivenName;
-				string npcTitle = Main.npc[partyGirl].TypeName;
-				Color pink = new Color(255, 110, 160);
-				WorldGen.BroadcastText(NetworkText.FromLiteral($"{npcName} the {npcTitle} has arrived to the party!"), pink);
-
 				// Put the Party Girl spawn on cooldown
-				// Will only ever turn back on when night occurs
+				// Reset cooldown needs to occur at every party
 				SpawnPartyGirlReady = false;
 			}
 		}
