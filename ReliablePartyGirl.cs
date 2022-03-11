d0da8d14c44d89277496a6c3cb4e5e35a39942be
Sheepish Shepherd <cameronscottmcdougal@gmail.com>
2022-03-11 12:31:29 -0500

    Updated for IEntitySource

--- ReliablePartyGirl.cs
+++ ReliablePartyGirl.cs
@@ -23,28 +23,24 @@ namespace ReliablePartyGirl
 				SpawnPartyGirlReady = true;
 			}
 			
-			// If a genuine party is occuring and there is no existing Party Girl, attempt to spawn one
+			// If a genuine party is occuring and there is no existing Party Girl, attempt to spawn one when ready
 			// GenuineParty assumes PartyIsUp is true
-			if (BirthdayParty.GenuineParty && NPC.FindFirstNPC(NPCID.PartyGirl) < 0) {
-				// If not on cooldown, spawn the Party Girl
-				if (SpawnPartyGirlReady) {
-					// Spawn a smoke cloud
-					for (int j = 0; j < 4; j++) {
-						Utils.PoofOfSmoke(new Vector2(Main.spawnTileX * 16, Main.spawnTileY * 16));
-					}
-
-					// Create a new Party Girl NPC
-					int partyGirl = NPC.NewNPC(Main.spawnTileX * 16, Main.spawnTileY * 16, NPCID.PartyGirl);
-
-					// Broadcast that a Party Girl has arrived
-					string npcName = Main.npc[partyGirl].GivenName;
-					string npcTitle = Main.npc[partyGirl].TypeName;
-					Color pink = new Color(255, 110, 160);
-					WorldGen.BroadcastText(NetworkText.FromLiteral($"{npcName} the {npcTitle} has arrived to the party!"), pink);
+			if (BirthdayParty.GenuineParty && NPC.FindFirstNPC(NPCID.PartyGirl) < 0 && SpawnPartyGirlReady) {
+				// Spawn a smoke cloud
+				for (int j = 0; j < 4; j++) {
+					Utils.PoofOfSmoke(new Vector2(Main.spawnTileX * 16, Main.spawnTileY * 16));
 				}
 
+				// Create a new Party Girl NPC
+				int partyGirl = NPC.NewNPC(NPC.GetSpawnSourceForTownSpawn(), Main.spawnTileX * 16, Main.spawnTileY * 16, NPCID.PartyGirl);
+
+				// Broadcast that a Party Girl has arrived
+				string npcName = Main.npc[partyGirl].GivenName;
+				string npcTitle = Main.npc[partyGirl].TypeName;
+				Color pink = new Color(255, 110, 160);
+				WorldGen.BroadcastText(NetworkText.FromLiteral($"{npcName} the {npcTitle} has arrived to the party!"), pink);
+
 				// Put the Party Girl spawn on cooldown
-				// Reset cooldown needs to occur at every party
 				SpawnPartyGirlReady = false;
 			}
 		}
